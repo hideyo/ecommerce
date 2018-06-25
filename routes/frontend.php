54c33eb8809b18b0542be002128cf716637e2b4f
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2018-06-25 21:08:46 +0200

    clean-up frontend route

--- routes/frontend.php
+++ routes/frontend.php
@@ -12,86 +12,79 @@ Route::get('/nieuws/{newsGroupSlug}', array('as' => 'news.group', 'uses' => 'New
 
 Route::get('/nieuws', array('as' => 'news.index', 'uses' => 'NewsController@getIndex'));
 
-Route::get('account/login', array('as' => 'account.login', 'uses' => 'AccountController@getLogin'));
 
-//Account    
-Route::post('account/login', array('as' => 'account.login', 'uses' => 'AccountController@postLogin'));
-Route::get('account/logout', 'AccountController@getLogout');
+Route::group(['prefix' => 'account'], function () {
 
-Route::get('account/forgot-password', 'AccountController@getForgotPassword');
-Route::post('account/forgot-password', 'AccountController@postForgotPassword');
+	Route::get('/login', array('as' => 'account.login', 'uses' => 'AccountController@getLogin'));
+	Route::post('/login', array('as' => 'account.login', 'uses' => 'AccountController@postLogin'));
+	
+	Route::get('/logout', 'AccountController@getLogout');
 
-Route::get('account/reset-password/{code}/{email}', 'AccountController@getResetPassword');
-Route::post('account/reset-password/{code}/{email}', 'AccountController@postResetPassword');
+	Route::get('/reset-password/{code}/{email}', 'AccountController@getResetPassword');
+	Route::post('/reset-password/{code}/{email}', 'AccountController@postResetPassword');
 
-Route::post('account/subscribe-newsletter', array('as' => 'account.newsletter.subscribe', 'uses' => 'AccountController@postSubscriberNewsletter'));
+	Route::post('/subscribe-newsletter', array('as' => 'account.newsletter.subscribe', 'uses' => 'AccountController@postSubscriberNewsletter'));
 
-Route::get('account/confirm/{code}/{email}', 'AccountController@getConfirm');
-Route::get('account/check-zipcode/{zipcode?}/{housenumber?}', 'AccountController@getZipcode');
+	Route::get('/confirm/{code}/{email}', 'AccountController@getConfirm');
+	Route::get('/check-zipcode/{zipcode?}/{housenumber?}', 'AccountController@getZipcode');
 
-Route::get('account/register', array('as' => 'account.register', 'uses' => 'AccountController@getRegister'));
-Route::post('account/register', array('as' => 'account.register', 'uses' => 'AccountController@postRegister'));
-Route::get('account/forgot-password', array('as' => 'account.forgot.password', 'uses' => 'AccountController@getForgotPassword'));
-Route::post('account/forgot-password', array('as' => 'account.forgot.password', 'uses' => 'AccountController@postForgotPassword'));
+	Route::get('/register', array('as' => 'account.register', 'uses' => 'AccountController@getRegister'));
+	Route::post('/register', array('as' => 'account.register', 'uses' => 'AccountController@postRegister'));
+	
+	Route::get('/forgot-password', array('as' => 'account.forgot.password', 'uses' => 'AccountController@getForgotPassword'));
+	Route::post('/forgot-password', array('as' => 'account.forgot.password', 'uses' => 'AccountController@postForgotPassword'));
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/account', 'AccountController@getIndex');
-    Route::get('/account/edit-account', 'AccountController@getEditAccount');
-    Route::post('/account/edit-account', 'AccountController@postEditAccount');
-    Route::get('/account/edit-address/{type}', 'AccountController@getEditAddress');
-    Route::post('/account/edit-address/{type}', 'AccountController@postEditAddress');
-    Route::get('/account/download-order/{orderId}', 'AccountController@getDownloadOrder');
+	Route::group(['middleware' => 'auth'], function () {
+	    Route::get('/', 'AccountController@getIndex');
+	    Route::get('/edit-account', 'AccountController@getEditAccount');
+	    Route::post('/edit-account', 'AccountController@postEditAccount');
+	    Route::get('/edit-address/{type}', 'AccountController@getEditAddress');
+	    Route::post('/edit-address/{type}', 'AccountController@postEditAddress');
+	    Route::get('/download-order/{orderId}', 'AccountController@getDownloadOrder');
+	});
 });
-
 
 
 Route::get('product/select-second-pulldown/{productId}/{leadingAttributeId}/{SecondAttributeId}', array('as' => 'product.select-second-pulldown', 'uses' => 'ProductController@getSelectLeadingPulldown'));
 Route::get('product/select-leading-pulldown/{productId}/{attributeId}', array('as' => 'product.select-leading-pulldown', 'uses' => 'ProductController@getSelectLeadingPulldown'));
 
-Route::get('cart/update-sending-method/{sendingMethodId}', array('as' => 'cart.update.sending.method', 'uses' => 'CartController@updateSendingMethod'));
-Route::get('cart/update-payment-method/{paymentMethodId}', array('as' => 'cart.update.payment.method', 'uses' => 'CartController@updatePaymentMethod'));
+Route::group(['prefix' => 'cart'], function () {
 
-Route::get('cart/checkout', array('as' => 'cart.checkout', 'uses' => 'CheckoutController@checkout'));
-Route::post('cart/checkout', array('as' => 'cart.checkout', 'uses' => 'CheckoutController@checkout'));
+	Route::get('/update-sending-method/{sendingMethodId}', array('as' => 'cart.update.sending.method', 'uses' => 'CartController@updateSendingMethod'));
+	Route::get('/update-payment-method/{paymentMethodId}', array('as' => 'cart.update.payment.method', 'uses' => 'CartController@updatePaymentMethod'));
 
+	Route::get('/checkout', array('as' => 'cart.checkout', 'uses' => 'CheckoutController@checkout'));
+	Route::post('/checkout', array('as' => 'cart.checkout', 'uses' => 'CheckoutController@checkout'));
 
-Route::post('cart/checkout-register', array('as' => 'cart.checkout-register', 'uses' => 'CheckoutController@postCheckoutRegister'));
-    Route::get('cart/edit-address/{type}', array('as' => 'cart.edit.address', 'uses' => 'CheckoutController@getEditAddress'));
-    Route::post('cart/edit-address/{type}', array('as' => 'cart.edit.address', 'uses' => 'CheckoutController@postEditAddress'));
- 
+	Route::post('/checkout-register', array('as' => 'cart.checkout-register', 'uses' => 'CheckoutController@postCheckoutRegister'));
+    Route::get('/edit-address/{type}', array('as' => 'cart.edit.address', 'uses' => 'CheckoutController@getEditAddress'));
+    Route::post('/edit-address/{type}', array('as' => 'cart.edit.address', 'uses' => 'CheckoutController@postEditAddress'));	 
 
-Route::post('cart/complete', array('as' => 'cart.complete', 'uses' => 'CheckoutController@postComplete'));
+	Route::post('/complete', array('as' => 'cart.complete', 'uses' => 'CheckoutController@postComplete'));
 
-Route::post('cart/checkout-login', array('as' => 'cart.checkout.login', 'uses' => 'CheckoutController@postCheckoutLogin'));
+	Route::post('/checkout-login', array('as' => 'cart.checkout.login', 'uses' => 'CheckoutController@postCheckoutLogin'));
 
+	Route::get('/summary-reload', array('as' => 'cart.summary.reload', 'uses' => 'CartController@getSummaryReload'));
+	    
+	Route::get('/total-reload', array('as' => 'cart.total-reload', 'uses' => 'CartController@getTotalReload'));
+	Route::get('/dialog', array('as' => 'cart.dialog', 'uses' => 'CartController@getBasketDialog'));
+	Route::get('/update-amount-product/{productId}/{amount}', array('as' => 'cart.update.amount.product', 'uses' => 'CartController@updateAmountProduct'));
+	Route::get('/delete-product/{productId}', array('as' => 'cart.delete.product', 'uses' => 'CartController@deleteProduct'));
+	Route::get('/', array('as' => 'cart.index', 'uses' => 'CartController@getIndex'));
 
-Route::get('cart/summary-reload', array('as' => 'cart.summary.reload', 'uses' => 'CartController@getSummaryReload'));
-    
-Route::get('cart/total-reload', array('as' => 'cart.total-reload', 'uses' => 'CartController@getTotalReload'));
-Route::get('cart/dialog', array('as' => 'cart.dialog', 'uses' => 'CartController@getBasketDialog'));
-Route::get('cart/update-amount-product/{productId}/{amount}', array('as' => 'cart.update.amount.product', 'uses' => 'CartController@updateAmountProduct'));
-Route::get('cart/delete-product/{productId}', array('as' => 'cart.delete.product', 'uses' => 'CartController@deleteProduct'));
-Route::get('cart', array('as' => 'cart.index', 'uses' => 'CartController@getIndex'));
+	//Cart
+	Route::post('/post-product/{productId}/{productCombinationId?}', array('as' => 'cart.add.product', 'uses' => 'CartController@postProduct'));
+	Route::get('/delete-product/{productId}', array('as' => 'cart.delete.product', 'uses' => 'CartController@deleteProduct'));
+
+});
+
 
 //product routes
 Route::get('{productCategorySlug}/{productId}/{productSlug}/{leadingAttributeId?}/{combinations?}', array('as' => 'product.item', 'uses' => 'ProductController@getIndex'));
 
-
 //productCategory routes
 Route::get('/{slug}', array('as' => 'product-category.item', 'uses' => 'ProductCategoryController@getItem'));
-
-//Cart
-Route::post('cart/post-product/{productId}/{productCombinationId?}', array('as' => 'cart.add.product', 'uses' => 'CartController@postProduct'));
-Route::get('cart/delete-product/{productId}', array('as' => 'cart.delete.product', 'uses' => 'CartController@deleteProduct'));
-
-
 
 
 //other
 Route::get('/', array('as' => 'index', 'uses' => 'BasicController@index'));
-
-
-
-
-
-
