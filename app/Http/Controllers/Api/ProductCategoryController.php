e64d58a749c8664ae37278b9313595ea98976e96
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2018-10-11 09:51:28 +0200

    mega clean up

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-
-
-use Hideyo\Ecommerce\Framework\Services\ProductCategory\ProductCategoryFacade as ProductCategoryService
+use Hideyo\Ecommerce\Framework\Services\ProductCategory\ProductCategoryFacade as ProductCategoryService;
 
 class ProductCategoryController extends Controller
 {
--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Hideyo\Ecommerce\Framework\Services\Product\ProductFacade as ProductService
-
+use Hideyo\Ecommerce\Framework\Services\Product\ProductFacade as ProductService;
 
 class ProductController extends Controller
 {
