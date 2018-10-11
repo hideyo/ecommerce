cb5f3995f37e91b0cde78580d2df2164c5769628
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2018-10-11 12:33:28 +0200

    mega clean up

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,34 +12,12 @@ let mix = require('laravel-mix');
  */
 
 
-mix.combine([
-    './bootstrap',
-    'node_modules/ekko-lightbox/dist/ekko-lightbox.js',
-    'node_modules/bootstrap-validator/dist/validator.js',
-	'resources/assets/js/frontend.js'
-], 'public/js/frontend.js');
-
-
-
-mix.combine([
-    './bootstrap',
-    'node_modules/bootbox/bootbox.js',
-    'node_modules/datatables/media/js/jquery.dataTables.js',
-    'node_modules/select2/dist/js/select2.js',
-    'node_modules/jstree/dist/jstree.min.js',
-    'node_modules/bootstrap-datepicker/js/bootstrap-datepicker.js',
-    'node_modules/bootstrap-validator/dist/validator.js',
-    'node_modules/summernote/dist/summernote.js',
-	'resources/assets/js/backend.js'
-], 'public/js/backend.js');
-
-
-
+mix.js('resources/assets/js/frontend.js', 'public/js');
+mix.js('resources/assets/js/backend.js', 'public/js');
 
 
 mix.sass('resources/assets/sass/frontend/frontend.scss', 'public/css')
 .sass('resources/assets/sass/backend/backend.scss', 'public/css/')
-
 .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts')
 .copy('node_modules/font-awesome/fonts', 'public/fonts')
 .copy('node_modules/roboto-fontface/fonts', 'public/fonts')
