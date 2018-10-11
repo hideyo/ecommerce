96f0ff382079aa7c1ef210b854c6e175db7d5805
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2018-10-11 10:43:34 +0200

    mega clean up

--- app/Http/Controllers/Backend/ProductAmountSeriesController.php
+++ app/Http/Controllers/Backend/ProductAmountSeriesController.php
@@ -9,7 +9,7 @@
  */
 
 use App\Http\Controllers\Controller;
-use Hideyo\Ecommerce\Framework\Services\Product\Entity\ProductAmountSeriesRepository;
+use Hideyo\Ecommerce\Framework\Services\Product\ProductAmountSeriesFacade as ProductAmountSeriesService;
 use Hideyo\Ecommerce\Framework\Services\Product\ProductFacade as ProductService;
 use Illuminate\Http\Request;
 use Notification;
@@ -18,18 +18,11 @@ use Form;
 
 class ProductAmountSeriesController extends Controller
 {
-    public function __construct(ProductAmountSeriesRepository $productAmountSeries) 
-    {
-        $this->productAmountSeries = $productAmountSeries;
-    }
-
     public function index(Request $request, $productId)
     {
         $product = ProductService::find($productId);
         if ($request->wantsJson()) {
-
-
-            $query = $this->productAmountSeries->getModel()->select(
+            $query = ProductAmountSeriesService::getModel()->select(
                 ['id', 'series_start', 'series_value', 'active','series_max']
             )->where('product_id', '=', $productId);
             
@@ -58,28 +51,14 @@ class ProductAmountSeriesController extends Controller
 
     public function store(Request $request, $productId)
     {
-        $result  = $this->productAmountSeries->create($request->all(), $productId);
- 
-        if (isset($result->id)) {
-            Notification::success('The product amount series is updated.');
-            return redirect()->route('product.product-amount-series.index', $productId);
-        }
-
-        if ($result) {
-            foreach ($result->errors()->all() as $error) {
-                \Notification::error($error);
-            }
-        } else {
-            \Notification::error('amount series already exist');
-        }
-        
-        return \redirect()->back()->withInput();
+        $result  = ProductAmountSeriesService::create($request->all(), $productId);
+        return ProductService::notificationRedirect(array('product.product-amount-series.index', $productId), $result, 'The product amount series is inserted.');
     }
 
     public function edit(Request $request, $productId, $id)
     {
         $product = ProductService::find($productId);
-        $productAmountSeries = $this->productAmountSeries->find($id);
+        $productAmountSeries = ProductAmountSeriesService::find($id);
 
         return view('backend.product-amount-series.edit')->with(
             array(
@@ -91,19 +70,13 @@ class ProductAmountSeriesController extends Controller
 
     public function update(Request $request, $productId, $id)
     {
-        $result  = $this->productAmountSeries->updateById($request->all(), $productId, $id);
-
-        if (!$result->id) {
-            return redirect()->back()->withInput()->withErrors($result->errors()->all());
-        }
-        
-        Notification::success('The product amount series is updated.');
-        return redirect()->route('product.product-amount-series.index', $productId);
+        $result  = ProductAmountSeriesService::updateById($request->all(), $productId, $id);
+        return ProductService::notificationRedirect(array('product.product-amount-series.index', $productId), $result, 'The product amount series is updated.');
     }
 
     public function destroy($productId, $id)
     {
-        $result  = $this->productAmountSeries->destroy($id);
+        $result  = ProductAmountSeriesService::destroy($id);
 
         if ($result) {
             Notification::success('The product amount series is deleted.');
